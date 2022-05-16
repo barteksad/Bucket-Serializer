6997e37ece74009d429bd279dcc0563e7889a3df
barteksad <barteksadlej@gmail.com>
2022-05-16 09:15:48 +0200

    set paths in src/copy-files.sh

--- src/copy-files.sh
+++ src/copy-files.sh
@@ -6,5 +6,6 @@ scp kernel/system/* minix:/usr/src/minix/kernel/system/
 scp sched/* minix:/usr/src/minix/servers/sched/
 
 scp include/minix/* minix:/usr/include/minix/*
+scp include/minix/* minix:/usr/src/minix/include/minix/*
 
 scp include/minix* minix:/usr/src/minix/lib/libsys/
