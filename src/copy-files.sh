44d5ee585c2bb743bc6b19a7f4e0e16f847460fe
barteksad <barteksadlej@gmail.com>
2022-05-19 22:43:04 +0200

    seems to be ok

--- src/copy-files.sh
+++ src/copy-files.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 scp kernel/proc.* minix:/usr/src/minix/kernel/
+scp kernel/proto.h minix:/usr/src/minix/kernel/proto.h
+scp kernel/system.* minix:/usr/src/minix/kernel/
 scp kernel/system/* minix:/usr/src/minix/kernel/system/
 
 scp sched/* minix:/usr/src/minix/servers/sched/
+scp pm/* minix:/usr/src/minix/servers/pm/
 
-scp include/minix/* minix:/usr/include/minix/*
-scp include/minix/* minix:/usr/src/minix/include/minix/*
+scp include/minix/* minix:/usr/src/minix/include/minix/
 
-scp include/minix* minix:/usr/src/minix/lib/libsys/
+scp lib/libsys/* minix:/usr/src/minix/lib/libsys/
+scp lib/libc/misc/* minix:/usr/src/lib/libc/misc/
+
+scp unistd.h minix:/usr/src/include/unistd.h
