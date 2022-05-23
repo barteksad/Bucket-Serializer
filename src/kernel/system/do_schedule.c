390060c11c8ad54fdbf53e5d572b01bb5c3ce8be
barteksad <barteksadlej@gmail.com>
2022-05-23 12:27:07 +0200

    added clear copy of all used files

---
+++ src/kernel/system/do_schedule.c
@@ -0,0 +1,33 @@
+#include "kernel/system.h"
+#include <minix/endpoint.h>
+#include "kernel/clock.h"
+
+/*===========================================================================*
+ *				do_schedule				     *
+ *===========================================================================*/
+int do_schedule(struct proc * caller, message * m_ptr)
+/* so_2022 */
+{
+	struct proc *p;
+	int proc_nr;
+	int priority, quantum, cpu, bucket;
+	
+	if (!isokendpt(m_ptr->m_lsys_krn_schedule.endpoint, &proc_nr))
+		return EINVAL;
+
+	p = proc_addr(proc_nr);
+
+	/* Only this process' scheduler can schedule it */
+	if (caller != p->p_scheduler)
+		return(EPERM);
+
+	/* Try to schedule the process. */
+	priority = BUCKET_Q;
+	quantum = m_ptr->m_lsys_krn_schedule.quantum;
+	cpu = m_ptr->m_lsys_krn_schedule.cpu;
+	bucket = m_ptr->m_lsys_krn_schedule.bucket;
+
+	// printf("do_schedule: priority %d, quantum %d, cpu %d, bucket %d\n", priority, quantum,cpu, bucket);
+
+	return sched_proc(p, priority, quantum, cpu, bucket);
+}
